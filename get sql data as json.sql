c88c6a1273df70a0cbb118697a14a414c2f02d34
vps4618 <vishwapraveen4618@gmail.com>
2022-08-24 22:33:51 +0530

    json

--- get sql data as json.sql
+++ get sql data as json.sql
@@ -1,21 +1,12 @@
 /****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (100000) [ITEM_ID]
+SELECT TOP (10000) [ITEM_ID]
       ,[ItemName]
-      ,[UnitOfMeasure]
-      ,[Batch]
-      ,[GROUP_ID]
       ,[Barcode]
       ,[Cost]
       ,[Price]
-      ,[ReorderPoint]
-      ,[VAT_Applicable]
-      ,[WarehouseID]
-      ,[PhotoFileName]
-      ,[Discount]
       ,[SinhalaName]
       ,[BillPrice]
       ,[Wholesale]
   FROM [dbAxia].[dbo].[iteminformation]
       FOR JSON PATH, 
         INCLUDE_NULL_VALUES
-GO
